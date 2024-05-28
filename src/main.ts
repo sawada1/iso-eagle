38d57c5a344252e9fb1e6b35fda5c4ae2959924a
khaled sawada <94247954+sawada1@users.noreply.github.com>
2024-05-28 17:16:26 +0300

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './assets/styles/main.scss';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -10,5 +10,16 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+app.use(vuetify);
 
 app.mount('#app')
